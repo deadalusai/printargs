fdfb34a1333c22aef314ab39469971bb97ea1692
Ben Fox <deadalus.ai@gmail.com>
2021-01-10 18:17:48 +1300

    Nix and Windows Invoke-Native functions

--- test.ps1
+++ test.ps1
@@ -19,21 +19,35 @@ function PrintArgs {
     }
 }
 
-function Invoke-Native {
+function Invoke-NativeNix {
+    param($Executable, $Arguments)
+    # Build an arguments string which is safe to pass directly to a native Linux/MacOs executable.
+    $Arguments = $Arguments | ForEach-Object {
+        $_ = $_ -Replace '(\\+)','$1$1' # Escape backslash chains.
+        $_ = $_ -Replace '"','\"'       # Escape internal quote marks.
+        "`"$_`""                        # Quote the argument.
+    }
+    # Use the stop-parsing symbol (--%) to ensure the argument string is passed along unchanged 
+    # See: https://docs.microsoft.com/en-us/powershell/module/microsoft.powershell.core/about/about_parsing?view=powershell-5.1
+    Invoke-Expression "& `"$Executable`" --% $($Arguments -Join ' ')"
+}
+
+function Invoke-NativeWindows {
     param($Executable, $Arguments)
     # Build an arguments string which follows the Windows command-line arguments string rules
     # See: https://docs.microsoft.com/en-us/previous-versions//17w5ykft(v=vs.85)?redirectedfrom=MSDN
     $Arguments = $Arguments | ForEach-Object {
-        $_ = $_ -Replace '(\\+)"','$1$1"' # Double-up all backslashes immediately preceeding quote marks.
-        $_ = $_ -Replace '(\\+)$','$1$1'  # Double-up all backslashes immediately preceeding the end of the string.
+        $_ = $_ -Replace '(\\+)"','$1$1"' # Escape backslash chains immediately preceeding quote marks.
+        $_ = $_ -Replace '(\\+)$','$1$1'  # Escape backslash chains immediately preceeding the end of the string.
         $_ = $_ -Replace '"','\"'         # Escape internal quote marks.
         "`"$_`""                          # Quote the argument.
     }
     # Use the stop-parsing symbol (--%) to ensure the argument string is passed along unchanged 
-    # See: https://docs.microsoft.com/en-us/powershell/module/microsoft.powershell.core/about/about_parsing?view=powershell-7.1
+    # See: https://docs.microsoft.com/en-us/powershell/module/microsoft.powershell.core/about/about_parsing?view=powershell-5.1
     Invoke-Expression "& `"$Executable`" --% $($Arguments -Join ' ')"
 }
 
 Test { PrintArgs $a $b $c $e --arg=$d }
-Test { Invoke-Native -Executable "$PSScriptRoot\target\debug\printargs" -Arguments @($a, $b, $c, $e, "--arg=$d") }
+Test { Invoke-NativeNix -Executable "$PSScriptRoot\target\debug\printargs" -Arguments @($a, $b, $c, $e, "--arg=$d") }
+Test { Invoke-NativeWindows -Executable "$PSScriptRoot\target\debug\printargs" -Arguments @($a, $b, $c, $e, "--arg=$d") }
 Test { & "$PSScriptRoot\target\debug\printargs" $a $b $c $e --arg=$d <# (this one doesn't work) #> }
