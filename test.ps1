6520d441efca92b285df95adab4b197058f49296
Ben Fox <deadalus.ai@gmail.com>
2021-01-10 18:24:15 +1300

    The escaping function appears to work on both platforms

--- test.ps1
+++ test.ps1
@@ -19,22 +19,9 @@ function PrintArgs {
     }
 }
 
-function Invoke-NativeNix {
+function Invoke-Native {
     param($Executable, $Arguments)
-    # Build an arguments string which is safe to pass directly to a native Linux/MacOs executable.
-    $Arguments = $Arguments | ForEach-Object {
-        $_ = $_ -Replace '(\\+)','$1$1' # Escape backslash chains.
-        $_ = $_ -Replace '"','\"'       # Escape internal quote marks.
-        "`"$_`""                        # Quote the argument.
-    }
-    # Use the stop-parsing symbol (--%) to ensure the argument string is passed along unchanged 
-    # See: https://docs.microsoft.com/en-us/powershell/module/microsoft.powershell.core/about/about_parsing?view=powershell-5.1
-    Invoke-Expression "& `"$Executable`" --% $($Arguments -Join ' ')"
-}
-
-function Invoke-NativeWindows {
-    param($Executable, $Arguments)
-    # Build an arguments string which follows the Windows command-line arguments string rules
+    # Build an arguments string which follows the C++ command-line argument quoting rules
     # See: https://docs.microsoft.com/en-us/previous-versions//17w5ykft(v=vs.85)?redirectedfrom=MSDN
     $Arguments = $Arguments | ForEach-Object {
         $_ = $_ -Replace '(\\+)"','$1$1"' # Escape backslash chains immediately preceeding quote marks.
@@ -48,6 +35,5 @@ function Invoke-NativeWindows {
 }
 
 Test { PrintArgs $a $b $c $e --arg=$d }
-Test { Invoke-NativeNix -Executable "$PSScriptRoot\target\debug\printargs" -Arguments @($a, $b, $c, $e, "--arg=$d") }
-Test { Invoke-NativeWindows -Executable "$PSScriptRoot\target\debug\printargs" -Arguments @($a, $b, $c, $e, "--arg=$d") }
+Test { Invoke-Native -Executable "$PSScriptRoot\target\debug\printargs" -Arguments @($a, $b, $c, $e, "--arg=$d") }
 Test { & "$PSScriptRoot\target\debug\printargs" $a $b $c $e --arg=$d <# (this one doesn't work) #> }
